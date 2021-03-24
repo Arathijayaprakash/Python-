0374e0e65b94f05b3494af8f084cf96151920892
Arathijayaprakash <arathiparambil@gmail.com>
2021-03-24 15:41:44 +0530

    cO3

---
+++ co3/graphics/sec_pack/sphere.py
@@ -0,0 +1,4 @@
+def area(r):
+    return (4*3.14*r*r)
+def perim(r):
+    return (4/3*(3.14*r*r*r))
---
+++ co3/import_area_perimeter.py
@@ -0,0 +1,11 @@
+from co3.graphics import Circle as c, Rectangle as r
+from co3.graphics.sec_pack import sphere as s, cuboid as cu
+
+print(c.area(2))
+print(c.peri(2))
+print(r.area(2,2))
+print(r.peri(2,2))
+print(cu.area(2,2,2))
+print(cu.perim(2,2,2))
+print(s.area(2))
+print(s.perim(2))
